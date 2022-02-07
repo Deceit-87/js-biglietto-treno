b6fe4ab4eab1364459f21c200c33eec11f470cc0
Deceit-87 <manuelfreund318@gmail.com>
2022-02-07 19:47:45 +0100

    calcolo js funzionante

--- js/app.js
+++ js/app.js
@@ -3,9 +3,9 @@
 // 3 sconto 
 
 const PREZZO_KM = 0.21;
+const total = document.getElementById('prezzo-finale');
 
-
-let lunghezzaTragitto = parseInt(prompt('quanti Km devi percorrere ?'));
+let lunghezzaTragitto = parseFloat(prompt('quanti Km devi percorrere ?'));
 
 console.log(lunghezzaTragitto);
 
@@ -13,25 +13,39 @@ let etaUtente =parseInt(prompt('quanti anni hai?'));
 
 console.log(etaUtente);
 
+if (isNaN(lunghezzaTragitto) || isNaN ( etaUtente ) ){
+    alert('i dati inseriti non sono corretti, inserire il formato giusto');
+}
 const COSTO_BIGLIETTO_INTERO = (lunghezzaTragitto) * (PREZZO_KM);
 
 console.log(COSTO_BIGLIETTO_INTERO)
+let sconto = 0;
 
 const SCONTO_20 = 0.2
 
-const SCONTO_80 = 0.8
+const SCONTO_40 = 0.4
 
 if (etaUtente < 18) {
-
-    let bigliettoSconto20  = COSTO_BIGLIETTO_INTERO - (COSTO_BIGLIETTO_INTERO * SCONTO_20);
-
-    console.log(bigliettoSconto20)
+ 
+    sconto = COSTO_BIGLIETTO_INTERO * SCONTO_20;
+    
+    console.log(sconto);
 
 }
 
 else if (etaUtente > 60) {
-    let bigliettoSconto80  = COSTO_BIGLIETTO_INTERO - (COSTO_BIGLIETTO_INTERO * SCONTO_80);
-
-    console.log(bigliettoSconto80)
-
+    sconto = COSTO_BIGLIETTO_INTERO * SCONTO_40;
+    
+    console.log(sconto);
+   
 }
+
+
+let costoBigliettoFinale = (COSTO_BIGLIETTO_INTERO - sconto).toFixed(2);
+
+
+
+total.innerHTML = `Totale Viaggio ${costoBigliettoFinale}  &euro;`;
+
+
+console.log(costoBigliettoFinale) 
