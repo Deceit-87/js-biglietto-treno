06b3e5fc438d5dedcc700bf7d88f48afb950abad
Deceit-87 <manuelfreund318@gmail.com>
2022-02-08 16:24:58 +0100

    aggiuta interruzione script error isnan

--- js/app.js
+++ js/app.js
@@ -3,27 +3,42 @@
 // 3 sconto 
 
 const PREZZO_KM = 0.21;
+
 const total = document.getElementById('prezzo-finale');
 
-let lunghezzaTragitto = parseFloat(prompt('quanti Km devi percorrere ?'));
-
-console.log(lunghezzaTragitto);
-
-let etaUtente =parseInt(prompt('quanti anni hai?'));
-
-console.log(etaUtente);
-
-if (isNaN(lunghezzaTragitto) || isNaN ( etaUtente ) ){
-    alert('i dati inseriti non sono corretti, inserire il formato giusto');
-}
-const COSTO_BIGLIETTO_INTERO = (lunghezzaTragitto) * (PREZZO_KM);
-
-console.log(COSTO_BIGLIETTO_INTERO)
 let sconto = 0;
 
 const SCONTO_20 = 0.2
 
 const SCONTO_40 = 0.4
+
+let lunghezzaTragitto = parseFloat(prompt('quanti Km devi percorrere ?'));
+
+
+
+
+
+
+if (isNaN(lunghezzaTragitto) ){
+    alert('i dati inseriti non sono corretti, inserire il formato giusto');
+
+
+
+}
+else if (!isNaN (lunghezzaTragitto)) {
+    var etaUtente = parseInt(prompt('quanti anni hai?'));
+    
+}
+
+else if (isNan(etaUtente)) {
+    alert('i dati inseriti non sono corretti, inserire il formato giusto');
+}
+
+
+const COSTO_BIGLIETTO_INTERO = (lunghezzaTragitto) * (PREZZO_KM);
+
+
+
 
 if (etaUtente < 18) {
  
